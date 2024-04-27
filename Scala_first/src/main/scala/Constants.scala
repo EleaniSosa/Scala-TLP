6c1746cadc4454c4f10c250464d7236dc6a21483
Eleani Sosa <71946405+EleaniSosa@users.noreply.github.com>
2024-04-27 08:41:17 -0600

    eliminar carpeta

--- Scala_first/src/main/scala/Constants.scala
+++
@@ -1,3 +0,0 @@
-object Constants {
-  val FILE_PATH="src/main/resources/automata.txt"
-}
--- Scala_first/src/main/scala/Transition.scala
+++
@@ -1,7 +0,0 @@
-class Transition(initialState:Char, letter:Char, finalState:Char) {
-  var firstState: Char = initialState
-  var char: Char = letter
-  var lastState : Char = finalState
-  override def toString(): String = "(" + firstState + "," + char + "," + lastState+ ")"
-
-}
